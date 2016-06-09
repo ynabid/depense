7fd02a0dbffbefd190e1cb23cf4c795a51ff2d51
Yassine ABID <yn.abid@gmail.com>
2016-06-09 01:53:51 +0000

    Second

--- depense.go
+++ depense.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	//	"html/template"
+	"container/list"
 	"database/sql"
+	"errors"
 	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"regexp"
 	"strconv"
 	"time"
 )
 
 type Depense struct {
-	date        int64
-	description string
-	montant     float64
+	Id          int64
+	Date        int64
+	Description string
+	Montant     float64
 }
 
 func openMysqlDB() (*sql.DB, error) {
@@ -28,17 +32,48 @@ func (d *Depense) insert() (sql.Result, error) {
 	}
 	return db.Exec(
 		"INSERT INTO depense (date,description,montant) VALUES (?,?,?)",
-		d.date,
-		d.description,
-		d.montant,
+		d.Date,
+		d.Description,
+		d.Montant,
 	)
 }
-func read(id int64) (Depense, error) {
+func getDepenseList(from, to int64) ([]Depense, error) {
+	var depenseList []Depense
+	l := list.New()
 	db, err := openMysqlDB()
 	if err != nil {
 		return nil, err
 	}
-	rows, err := db.query(
+	rows, err := db.Query(
+		"SELECT id,date,montant,description from depense WHERE date >= ? and date <= ? ORDER BY date",
+		from,
+		to,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var d Depense
+		rows.Scan(&d.Id, &d.Date, &d.Montant, &d.Description)
+		l.PushBack(d)
+	}
+	depenseList = make([]Depense, l.Len())
+	i := 0
+	for e := l.Front(); e != nil; e = e.Next() {
+		depenseList[i] = e.Value.(Depense)
+		i++
+	}
+	defer rows.Close()
+	return depenseList, nil
+}
+
+func read(id int64) (*Depense, error) {
+	db, err := openMysqlDB()
+	if err != nil {
+		return nil, err
+	}
+	rows, err := db.Query(
 		"SELECT date,montant,description from depense WHERE id=?",
 		id,
 	)
@@ -48,8 +83,8 @@ func read(id int64) (Depense, error) {
 	}
 	rows.Next()
 	var d Depense
-	rows.scan(&d.date, &d.montant, &d.description)
-	return d
+	rows.Scan(&d.Date, &d.Montant, &d.Description)
+	return &d, nil
 }
 
 func load(file string) ([]byte, error) {
@@ -60,59 +95,130 @@ func load(file string) ([]byte, error) {
 	return body, nil
 }
 
+/*
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := load("template/index.html")
-	if err != nil {
-		http.NotFound(w, r)
+  body, err := load("template/index.html")
+  if err != nil {
+    http.NotFound(w, r)
+  }
+  fmt.Fprintf(w, "%s", body)
+  //	renderTemplate(w, "edit", p)
+}
+*/
+func parseDepense(values url.Values) (*Depense, error) {
+	var depense Depense
+
+	t, err := time.Parse("2006-01-01", values["date"][0])
+	if err == nil {
+		depense.Date = t.Unix()
+	} else {
+		return nil, err
 	}
-	fmt.Fprintf(w, "%s", body)
-	//	renderTemplate(w, "edit", p)
+
+	depense.Description = values["description"][0]
+	depense.Montant, err = strconv.ParseFloat(values["montant"][0], 64)
+	if err != nil {
+		return nil, err
+	}
+	if depense.Montant <= 0 {
+		return nil, errors.New("Montant doit être >= 0")
+	}
+	return &depense, nil
 }
 func depenseHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
-		var depense Depense
-
-		t, err := time.Parse("2006-01-01", r.FormValue("date"))
-		if err == nil {
-			depense.date = t.Unix()
-		}
-		depense.description = r.FormValue("description")
-		depense.montant, err = strconv.ParseFloat(r.FormValue("montant"), 64)
+		r.ParseForm()
+		depense, err := parseDepense(r.PostForm)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-		if depense.montant <= 0 {
-			http.Error(w, "Montant doit être >= 0", http.StatusBadRequest)
+			return
 		}
 		result, err := depense.insert()
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		} else {
 			id, err := result.LastInsertId()
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
 			}
-			w.Header().Add("Location", "/depense/"+string(id))
+			w.Write([]byte("OK"))
+			w.Header().Add("Location", r.URL.Path+strconv.FormatInt(id, 10))
 			w.WriteHeader(http.StatusCreated)
 		}
 	} else if r.Method == "GET" {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil && len(m) > 2 {
+			http.NotFound(w, r)
+			return
+		}
+		id, err := strconv.ParseInt(m[2], 10, 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		d, err := read(id)
 
+		if err != nil {
+			http.NotFound(w, r)
+		}
+		b, err := json.Marshal(d)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Header().Add("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
+	}
+}
+
+func parseListDepense(values url.Values) (int64, int64, error) {
+	from, err := time.Parse("2006-01-01", values["from"][0])
+	if err != nil {
+		return 0, 0, err
+	}
+	to, err := time.Parse("2006-01-01", values["to"][0])
+	if err != nil {
+		return from.Unix(), 0, err
+	}
+	return from.Unix(), to.Unix(), nil
+}
+
+func depenseListHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		r.ParseForm()
+		from, to, err := parseListDepense(r.PostForm)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		depenseList, err := getDepenseList(from, to)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		} else {
+			b, err := json.Marshal(depenseList)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			w.Header().Add("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write(b)
+		}
 	}
 }
 
 /*func renderTemplate(w http.ResponseWriter, file string) {
-	t, err := template.ParseFiles("template/" + file)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+  t, err := template.ParseFiles("template/" + file)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  err = t.Execute(w, p)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
 }
 */
-var validPath = regexp.MustCompile("^/(depense)/[a-zA-Z0-9]*")
+var validPath = regexp.MustCompile("^/api/(depense|depenseList)/([a-zA-Z0-9]*)$")
 
 func makeHandler(fn func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -126,18 +232,23 @@ func makeHandler(fn func(w http.ResponseWriter, r *http.Request)) http.HandlerFu
 }
 
 func main() {
-	dir := "/home/yassine/Dev/godev/src/github.com/ynabid/mesrouf/"
+	dir := "/home/yassine/Dev/godev/src/github.com/ynabid/depense/"
 	http.Handle(
-		"/mesrouf/res/",
+		"/depense/",
 		http.StripPrefix(
-			"/mesrouf/res/",
+			"/depense/",
 			http.FileServer(http.Dir(dir+"res/")),
 		),
 	)
 	http.HandleFunc(
-		"/depense/",
+		"/api/depense/list",
+		depenseListHandler,
+	)
+
+	http.HandleFunc(
+		"/api/depense/",
 		makeHandler(depenseHandler),
 	)
+
 	http.ListenAndServe(":8080", nil)
-	fmt.Println("Test")
 }
